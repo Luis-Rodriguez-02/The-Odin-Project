25ba7cfbb0f5296674ea622ac097486bee2d07d2
Luis Rodriguez <106412395+Luis-Rodriguez-02@users.noreply.github.com>
2024-08-14 14:25:54 -0400

    add variables-and-operators lesson

---
+++ javascript-basics/variables-and-operators/script.js
@@ -0,0 +1,41 @@
+// This section contains a general overview of topics that you will learn in this lesson.
+
+// How do you declare a variable?
+
+// What are three different ways to declare a variable?
+
+// Which one should you use when?
+
+// What are the rules for naming variables?
+
+// What are operators, operands, and operations?
+
+// What is concatenation and what happens when you add numbers and strings together?
+
+// What are the different types of operators in JavaScript?
+
+// What is the difference between == and ===?
+
+// What are operator precedence values?
+
+// What are the increment/decrement operators?
+
+// What is the difference between prefixing and postfixing them?
+
+// What are assignment operators?
+
+// What is the Unary Plus Operator?
+
+
+let user = 'John';
+let age = 25
+let message = 'Hello';
+
+let admin;
+let namE = 'John'
+admin = namE
+alert(namE)
+
+let our_Planet = 'earth';
+let currentVisitor = 'user'
+
