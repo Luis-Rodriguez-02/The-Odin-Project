96ce986b65d16fb426cdd3156a547ed92ed5a555
Luis Rodriguez <106412395+Luis-Rodriguez-02@users.noreply.github.com>
2024-08-16 23:21:25 -0400

    add js intro notes

--- javascript-basics/data-types-and-conditionals/script.js
+++ javascript-basics/data-types-and-conditionals/script.js
@@ -1,14 +1,18 @@
-"" + 1 + 0 // 1
-"" - 1 + 0  // -1
-true + false // 1
-6 / "3" // 2 
-"2" * "3" // 6
-4 + 5 + "px" //9px
-"$" + 4 + 5 //$45
-"4" - 2 //2
-"4px" - 2 // nan
-"  -9  " + 5 // -95
-"  -9  " - 5  //-14
-null + 1 // 1
-undefined + 1 // nan
-" \t \n" - 2 // -2
+/*
+
+This section contains a general overview of topics that you will learn in this lesson:
+
+- Name the eight data types in JavaScript.
+- Understand the difference between single, - double, and backtick quotes.
+- Embed a variable/expression in a string.
+- Understand what a method is.
+- Name the three logical operators.
+- Understand what the comparison operators are.
+- Understand what conditionals are.
+- Understand what nesting is.
+- Understand what truthy and falsy values are.
+
+// Questions
+
+
+*/
