ac8345f1e0b7cbcac7ed3b1960fa1608cfbdc505
Luis Rodriguez <106412395+Luis-Rodriguez-02@users.noreply.github.com>
2024-08-17 12:45:10 -0400

    update

--- javascript-basics/variables-and-operators/script.js
+++ javascript-basics/variables-and-operators/script.js
@@ -39,3 +39,20 @@ alert(namE)
 let our_Planet = 'earth';
 let currentVisitor = 'user'
 
+
+
+"" + 1 + 0 // 1
+"" - 1 + 0  // -1
+true + false // 1
+6 / "3" // 2 
+"2" * "3" // 6
+4 + 5 + "px" //9px
+"$" + 4 + 5 //$45
+"4" - 2 //2
+"4px" - 2 // nan
+"  -9  " + 5 // -95
+"  -9  " - 5  //-14
+null + 1 // 1
+undefined + 1 // nan
+" \t \n" - 2 // -2
+
