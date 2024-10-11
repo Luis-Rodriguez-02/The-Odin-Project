d000153a3c9c3ec925e80e401f9ad09ef0909818
Luis Rodriguez <106412395+Luis-Rodriguez-02@users.noreply.github.com>
2024-10-11 12:27:44 -0400

    finish mdn shopping list

--- javascript-basics/dom-manipulation-and-events/mdn-shopping-list/script.js
+++ javascript-basics/dom-manipulation-and-events/mdn-shopping-list/script.js
@@ -1,6 +1,6 @@
-const unorderedList = document.querySelector("ul");
-const input = document.querySelector("item");
-const buttons = document.querySelectorAll("button");
+const unorderedList = document.querySelector("#list");
+const input = document.querySelector("#item");
+const buttons = document.querySelectorAll("#button");
 
 buttons.forEach((button) => {
   button.addEventListener("click", buttonClick);
@@ -8,4 +8,16 @@ buttons.forEach((button) => {
 
 function buttonClick() {
   let currentValue = input.value;
+  const listItem = document.createElement("li");
+  const spanItem = document.createElement("span");
+  spanItem.textContent = currentValue;
+  const delButton = document.createElement("button");
+  delButton.textContent = "Delete";
+
+  delButton.addEventListener("click", () => listItem.remove());
+  listItem.appendChild(spanItem);
+  listItem.appendChild(delButton);
+  unorderedList.appendChild(listItem);
+  input.value = "";
+  document.querySelector("input").focus();
 }
